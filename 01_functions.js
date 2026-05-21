bafc266637db99825e5a5e1abce0fe4acc8730c6
Panchal Shantanu <spanchal112000@gmail.com>
2026-05-21 21:27:57 +0530

    Add files via upload

---
+++ 01_functions.js
@@ -0,0 +1,19 @@
+
+function sayMyName(){
+    console.log('S')
+    console.log('H')
+    console.log('A')
+    console.log('N')
+    console.log('T')
+    console.log('A')
+    console.log('N')
+    console.log('U')
+}
+
+// sayMyName()
+
+function addTwoNumber(num1 , num2){
+    console.log(num2 + num2)
+}
+
+addTwoNumber(2,2)
