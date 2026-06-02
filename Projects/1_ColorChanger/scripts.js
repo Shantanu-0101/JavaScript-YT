d6702acbe5cc75f88e87221a9d59a43ae41a4108
Panchal Shantanu <spanchal112000@gmail.com>
2026-06-02 22:32:05 +0530

    Simple Js Project : Background Color Changer

---
+++ Projects/1_ColorChanger/scripts.js
@@ -0,0 +1,19 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector('body')
+
+button.forEach(function(button){
+    button.addEventListener('click', function(e){
+        if (e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'orange'){
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'pink'){
+            body.style.backgroundColor = e.target.id
+        }
+    })
+})
